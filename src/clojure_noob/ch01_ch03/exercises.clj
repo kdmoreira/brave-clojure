4d3f3434a9ae94c4f16e0385631d8866e1f31d4f
Karina Moreira <kakatinosa@hotmail.com>
2022-04-08 19:49:18 -0300

    Functions

--- src/clojure_noob/ch01_ch03/exercises.clj
+++ src/clojure_noob/ch01_ch03/exercises.clj
@@ -142,7 +142,7 @@ dog-names
 (conj [1 2 3] 4)
 ; => (1 2 3 4)
 
-;LISTS
+; LISTS
 '(1 2 3 4)
 
 (nth '(:a :b :c) 0)
@@ -156,7 +156,6 @@ dog-names
 ; => (4 1 2 3)
 
 ; SETS
-
 #{"hello" 1 :bye}
 
 (hash-set "hello" 1 :bye)
@@ -183,3 +182,156 @@ dog-names
 
 (get #{1 2} 3)
 ; => nil
+
+; FUNCTIONS
+(first [1 2 3])
+
+(or + -)
+(or - +)
+
+((or + -) 1 2 3)
+
+((and (= 1 1) +) 1 2 3)
+; => 6
+
+((first [+ 0]) 1 2 3)
+; => 6
+
+(or (= 1 (+ 0 1)) (= 0 (+ 0 1)))
+; => true
+
+(and (= 1 (+ 0 1)) (= 0 (+ 0 1)))
+; => false
+
+(inc 1)
+
+(map inc [1 2 3])
+
+(+ (inc 2) (/ 10 (- 4 2)))
+; => 8
+
+(defn say-hi
+  "Returns a greeting"
+  [name]
+  (str "Hello, " name "!"))
+
+(say-hi "Karina")
+
+; ARITY OVERLOADING
+(defn operations
+  ([n1]
+   (str "The next number is " (inc n1)))
+  ([n1 n2]
+   (str "The sum of both numbers is " (+ n1 n2)))
+  ([n1 n2 n3]
+   (str "The sum of the first two numbers is "
+        (if (= n3 (+ n1 n2))
+          (str "equal to " n3 "!")
+          (str "not equal to " n3 "!")))))
+
+(operations 1)
+(operations 1 2)
+(operations 1 2 3)
+(operations 1 2 4)
+
+(defn animal-sounds
+  "Describes animal sounds"
+  ([pet sound]
+   (str "The " pet " goes " sound))
+  ([pet]
+   (animal-sounds pet "zZzZz")))
+
+(animal-sounds "dog" "woof")
+(animal-sounds "dog")
+
+; REST PARAMETER
+(defn thing-in-basket
+  [thing]
+  (str thing " goes into the basket..."))
+
+(thing-in-basket "an orange")
+
+(defn stuff-in-basket
+  [& stuff]
+  (map thing-in-basket stuff))
+
+(stuff-in-basket "an apple", "an umbrella", "a can")
+
+(defn favorite-things
+  [name & things]
+  (str "Hi, I'm " name " and here are my favorite things: "
+       (clojure.string/join ", " things)))
+
+(favorite-things "Karina" "books" "art" "films")
+
+; DESTRUCTURING
+(defn choices
+  [[first second & unimportant]]
+  (println (str "My first choice is " first))
+  (println (str "My second choice is " second))
+  (println (str "My unimportant choices are: "
+                (clojure.string/join ", " unimportant))))
+
+(choices ["pineapple" "melon" "manga" "apple"])
+
+(defn pet-birds-v1
+  [{conure :conure budgie :budgie}]
+  (println (str "My conure is called " conure))
+  (println (str "My budgie is called " budgie)))
+
+(pet-birds-v1 {:conure "Duda" :budgie "Blue"})
+
+(defn pet-birds-v2
+  [{:keys [conure budgie]}]
+  (println (str "My conure is called " conure))
+  (println (str "My budgie is called " budgie)))
+
+(pet-birds-v2 {:conure "Duda" :budgie "Blue"})
+
+(def birds {:conure "Henri" :budgie "Beau" :macaw "Laila" :cockatiel "Duke"})
+
+(defn pet-birds-v3
+  [{:keys [conure budgie] :as birds}]
+  (println (str "I have many birds: " birds))
+  (println (str "My conure is called " conure))
+  (println (str "My budgie is called " budgie)))
+
+(pet-birds-v3 birds)
+
+(defn number-comparison
+  [x]
+  (if (> x 10)
+    "This number is greater than 10"
+    "This is a small number"))
+
+(number-comparison 2)
+
+; ANONYMOUS FUNCTIONS
+(map (fn [names] (str "Hi, " names))
+     ["Darth Vader" "Mr. Magoo"])
+
+((fn [x] (inc x)) 5)
+((fn [x y] (+ x y)) 1 2)
+
+(def square-multiplier (fn [x] (* x x)))
+(square-multiplier 5)
+
+#(* % 3)
+
+(#(* % 3) 8)
+; => 24
+
+(map #(* % 2) [1 2])
+(map #(str "Hello, " %) ["James" "John"])
+(#(* %1 %2) 2 5)
+(#(identity %&) 1 "x" :a)
+
+; RETURNING FUNCTIONS
+(defn multiplier-maker
+  "Create a custom multiplier"
+  [multiply-by]
+  #(* % multiply-by))
+
+(def by5 (multiplier-maker 5))
+
+(by5 3)
