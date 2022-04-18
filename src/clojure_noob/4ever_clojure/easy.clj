5bd44eed845f089f551de0d87369d5390a356631
Karina Moreira <kakatinosa@hotmail.com>
2022-04-18 20:16:00 -0300

    4ever-clojure 20 to 22

--- src/clojure_noob/4ever_clojure/easy.clj
+++ src/clojure_noob/4ever_clojure/easy.clj
@@ -14,3 +14,38 @@
 (= (my-last [1 2 3 4 5]) 5)
 (= (my-last '(5 4 3)) 3)
 (= (my-last ["b" "c" "d"]) "d")
+
+; P20
+(defn penultimate
+  ([sequence]
+   (if (= (count (rest sequence)) 1)
+     (first sequence)
+     (recur (rest sequence)))))
+
+(= (penultimate [1 2 3 4 5]) 4)
+(= (penultimate ["a" "b" "c"]) "b")
+(= (penultimate [[1 2] [3 4]]) [1 2])
+
+; P21
+(defn nth-element [sequence nth]
+  (last (take (+ nth 1) sequence)))
+
+(= (nth-element '(4 5 6 7) 2) 6)
+(= (nth-element [:a :b :c] 0) :a)
+(= (nth-element [1 2 3 4] 1) 2)
+(= (nth-element '([1 2] [3 4] [5 6]) 2) [5 6])
+
+; P22
+(defn count-seq [seq]
+  (loop [iteration 0 sequence seq]
+     (if (empty? sequence)
+       iteration
+       (recur (inc iteration) (rest sequence)))))
+
+(count-seq '(1 2 3 3 1))
+
+(= (count-seq '(1 2 3 3 1)) 5)
+(= (count-seq "Hello World") 11)
+(= (count-seq [[1 2] [3 4] [5 6]]) 3)
+(= (count-seq '(13)) 1)
+(= (count-seq '(:a :b :c)) 3)
