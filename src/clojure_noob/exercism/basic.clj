f3929846bafea53f124c103f08143cd5a5e7f799
Karina Moreira <karina.moreira@nubank.com.br>
2022-07-18 19:43:07 -0300

    Exercism Lists

--- src/clojure_noob/exercism/basic.clj
+++ src/clojure_noob/exercism/basic.clj
@@ -30,3 +30,43 @@
    Returns how many minutes in total you've worked on cooking the lasagna"
   [num-layers actual-time]
   (+ actual-time (prep-time num-layers)))
+
+; EX3 - TRACKS ON TRACKS ON TRACKS
+
+(defn new-list
+  "Creates an empty list of languages to practice."
+  []
+  '())
+
+(defn add-language
+  "Adds a language to the list."
+  [lang-list lang]
+  (conj lang-list lang))
+
+(defn first-language
+  "Returns the first language on the list."
+  [lang-list]
+  (first lang-list))
+
+(defn remove-language
+  "Removes the first language added to the list."
+  [lang-list]
+  (pop lang-list))
+
+(defn count-languages
+  "Returns the total number of languages on the list."
+  [lang-list]
+  (count lang-list))
+
+(defn learning-list
+  "Creates an empty list, adds Clojure and Lisp, removes Lisp, adds
+  Java and JavaScript, then finally returns a count of the total number
+  of languages."
+  []
+  (-> (new-list)
+      (add-language "Clojure")
+      (add-language "Lisp")
+      (remove-language)
+      (add-language "Java")
+      (add-language "JavaScript")
+      (count-languages)))
--- test/clojure_noob/exercism/basic_test.clj
+++ test/clojure_noob/exercism/basic_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure-noob.exercism.basic :refer :all]
             [clojure.test :refer :all]))
 
+; EX 2 - LUCIAN LUSCIOU`S LASAGNA
+
 (deftest expected-time-test
   (testing "returns the expected time which is 40"
     (is (= 40 expected-time))))
@@ -22,8 +24,41 @@
 
 (deftest total-time-test
   (testing "returns the time spent working in the lasagna (remaining + prep time)"
-    (are [num-layers actual-time total] 
+    (are [num-layers actual-time total]
          (= total (total-time num-layers actual-time))
       1, 0, 2
       2, 10, 14
       3, 40, 46)))
+
+; EX 3 - TRACKS ON TRACKS ON TRACKS
+
+(deftest new-list-test
+  (testing "Returns an empty list"
+    (is (= '() (new-list)))))
+
+(deftest add-language-test
+  (testing "Adds a language to the beginning of the list"
+    (is (= '("JavaScript" "Clojurescript")
+           (add-language '("Clojurescript") "JavaScript")))))
+
+(deftest first-language-test
+  (testing "Returns the last added language"
+    (is (= "Haskell" (first-language '("Haskell" "Python"))))))
+
+(deftest remove-language-test
+  (testing "Removes the first language from the list"
+    (is (= '("Racket" "Scheme") 
+           (remove-language '("Common Lisp" "Racket" "Scheme"))))))
+
+(deftest count-languages-test
+  (testing "Counts the language in the list"
+    (are [count list]
+         (= count (count-languages list))
+      2, '("Racket" "Scheme")
+      4, '("C#" "Racket" "Rust" "Ruby"))))
+
+(deftest learning-list-test
+  (testing "Creates an empty list, adds Clojure and Lisp, removes Lisp, adds
+  Java and JavaScript, then finally returns a count of the total number
+  of languages")
+  (is (= 3 (learning-list))))
